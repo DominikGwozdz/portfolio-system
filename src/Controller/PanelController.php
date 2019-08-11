8ffcc119d79607db11e8799ca8dacc03c2d36cc3
Dominik Gwozdz <dwgwozdz@gmail.com>
2019-08-11 10:37:36 +0200

    added new controller and view - panel

---
+++ src/Controller/PanelController.php
@@ -0,0 +1,19 @@
+<?php
+namespace App\Controller;
+
+use App\Controller\AppController;
+
+/**
+ * Panel Controller
+ *
+ *
+ * @method \App\Model\Entity\Panel[]|\Cake\Datasource\ResultSetInterface paginate($object = null, array $settings = [])
+ */
+class PanelController extends AppController
+{
+
+    public function index()
+    {
+    }
+
+}
---
+++ src/Template/Panel/index.ctp
@@ -0,0 +1,5 @@
+<?php
+//Nie wyswietlaj tego co jest w default.ctp
+$this->layout = false;
+
+?>
