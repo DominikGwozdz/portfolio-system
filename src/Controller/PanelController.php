2d6d2ca147d45bea3c95d69fe858380e6186185d
Dominik Gwozdz <dwgwozdz@gmail.com>
2019-08-20 21:53:16 +0200

    load component and existing entity. Then upload function added

--- src/Controller/PanelController.php
+++ src/Controller/PanelController.php
@@ -11,6 +11,13 @@ use App\Controller\AppController;
  */
 class PanelController extends AppController
 {
+    public function initialize()
+    {
+        parent::initialize();
+
+        $this->loadModel('TopSliderHomepage');
+        $this->loadComponent('Flash');
+    }
 
     public function index()
     {
@@ -31,12 +38,31 @@ class PanelController extends AppController
     public function upload()
     {
         $imageSentFromForm = $this->request->getData(['image_path']);
+        $titleFromForm = $this->request->getData('title');
+        $descriptionFromForm = $this->request->getData('description');
+        $isVisibleFromForm = $this->request->getData('is_visible');
+        if(empty($isVisibleFromForm)) {
+            $isVisibleFromForm = '0';
+        }
         $uploadPath = 'assets/top_slider_homepage/';
-        if(!empty($imageSentFromForm)) {
+        if(!empty($imageSentFromForm) && !empty($titleFromForm) && !empty($descriptionFromForm)) {
             $imageName = $imageSentFromForm['name'];
             $pathToUploadedImage = $uploadPath.$imageName;
             if (move_uploaded_file($imageSentFromForm['tmp_name'],$pathToUploadedImage))
             {
+                $saveInDb = $this->TopSliderHomepage->newEntity();
+                $saveInDb->title = $titleFromForm;
+                $saveInDb->description = $descriptionFromForm;
+                $saveInDb->url = 'top_slider_homepage/' . $imageName;
+                $saveInDb->is_visible = $isVisibleFromForm;
+                if($this->TopSliderHomepage->save($saveInDb))
+                {
+                    $this->Flash->success(__('Zdjęcie zostało poprawnie załadowane!'));
+                } else {
+                    $this->Flash->error(__('Nie udało się wgrac zdjęcia!'));
+                }
+
+
                 $this->redirect('/panel/slider');
             }
 
