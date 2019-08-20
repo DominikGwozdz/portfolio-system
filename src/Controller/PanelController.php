a096d83e2470041f7fca74486d22ec697f6e2763
Dominik Gwozdz <dwgwozdz@gmail.com>
2019-08-20 15:11:20 +0200

    Added uploading files to proper directory

--- src/Controller/PanelController.php
+++ src/Controller/PanelController.php
@@ -23,7 +23,24 @@ class PanelController extends AppController
     }
     public function slider()
     {
+        $top_slider_homepage = $this->loadModel('TopSliderHomepage');
+        $top_slider_homepage = $top_slider_homepage->find();
 
+        $this->set("top_slider_homepage", $top_slider_homepage);
+    }
+    public function upload()
+    {
+        $imageSentFromForm = $this->request->getData(['image_path']);
+        $uploadPath = 'assets/top_slider_homepage/';
+        if(!empty($imageSentFromForm)) {
+            $imageName = $imageSentFromForm['name'];
+            $pathToUploadedImage = $uploadPath.$imageName;
+            if (move_uploaded_file($imageSentFromForm['tmp_name'],$pathToUploadedImage))
+            {
+                $this->redirect('/panel/slider');
+            }
+
+        }
     }
 
 
--- src/Template/Panel/slider.ctp
+++ src/Template/Panel/slider.ctp
@@ -11,5 +11,17 @@ $this->layout = 'panel';
         <h1 class="h3 mb-0 text-gray-800">Górny slider</h1>
     </div>
 
+    <p class="pt-5 pb-5">Wszystkie zdjęcia w sliderze:</p>
+    <?php foreach($top_slider_homepage as $slider): ?>
+
+        <img src="/assets/<?= $slider->url ?>" class="img-fluid w-25" />
+    <?php endforeach; ?>
+
+    <p class="pt-5 pb-5">Dodaj nowe zdjęcia:</p>
+
+    <?= $this->Form->create("top_slider_homepage", ['url' => '/panel/upload', 'enctype' => 'multipart/form-data']) ?>
+    <?= $this->Form->control('image_path', ['type' => 'file', 'label' => __('Wybierz zdjęcie: ')]) ?>
+    <?= $this->Form->control('Wyślij zdjęcie', ['type' => 'submit']) ?>
+    <?= $this->Form->end() ?>
 </div>
 <!-- /.container-fluid -->
