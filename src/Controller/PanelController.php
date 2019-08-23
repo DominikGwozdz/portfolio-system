e6cdcedd840065d05c404ff77d9c99881be84c90
Dominik Gwozdz <dwgwozdz@gmail.com>
2019-08-23 20:06:13 +0200

    added new methods to controller Panel - about favourites upload and show all

--- src/Controller/PanelController.php
+++ src/Controller/PanelController.php
@@ -113,5 +113,47 @@ class PanelController extends AppController
         $this->redirect('/panel/slider/');
     }
 
+    //favourites footer
+    public function favouritesFooter() {
+        $favourites_footer = $this->loadModel('FavouritesFooter');
+        $favourites_footer = $favourites_footer->find();
+
+        $this->set("favourites_footer", $favourites_footer);
+    }
+
+    public function favouritesUpload() {
+        $this->loadModel('FavouritesFooter');
+
+        $imageSentFromForm = $this->request->getData(['image_path']);
+        $isVisibleFromForm = $this->request->getData('is_visible');
+        if(empty($isVisibleFromForm)) {
+            $isVisibleFromForm = '0';
+        }
+        $uploadPath = 'assets/favourites_footer/';
+        if(!empty($imageSentFromForm)) {
+            $imageName = $imageSentFromForm['name'];
+            $imageName = str_replace(" ", "_", $imageName);
+            $imageName = strtolower($imageName);
+
+            $pathToUploadedImage = $uploadPath.$imageName;
+            if (move_uploaded_file($imageSentFromForm['tmp_name'],$pathToUploadedImage))
+            {
+                $saveInDb = $this->FavouritesFooter->newEntity();
+                $saveInDb->url = 'favourites_footer/' . $imageName;
+                $saveInDb->is_visible = $isVisibleFromForm;
+                if($this->FavouritesFooter->save($saveInDb))
+                {
+                    $this->Flash->success(__('Zdjęcie zostało poprawnie załadowane!'));
+                } else {
+                    $this->Flash->error(__('Nie udało się wgrac zdjęcia!'));
+                }
+
+
+                $this->redirect('/panel/favourites_footer');
+            }
+
+        }
+    }
+
 
 }
