aee01d8999ca5c53e9c9bcb6e748b4608dc16ecf
Dominik Gwozdz <dwgwozdz@gmail.com>
2019-08-26 23:21:58 +0200

    added simple empty contact controller

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,30 @@
+<?php
+namespace App\Controller;
+
+use App\Controller\AppController;
+use Cake\ORM\TableRegistry;
+
+/**
+ * Contact Controller
+ *
+ *
+ * @method \App\Model\Entity\Contact[]|\Cake\Datasource\ResultSetInterface paginate($object = null, array $settings = [])
+ */
+class ContactController extends AppController
+{
+    public function initialize()
+    {
+        parent::initialize();
+
+        $this->loadComponent('Flash');
+    }
+
+    public function index()
+    {
+        $this->render('index');
+    }
+
+
+
+
+}
