53a71f73af5bcf133c40707ebb65b7f5f2e14689
Dominik Gwozdz <dwgwozdz@gmail.com>
2019-09-23 22:46:46 +0200

    added bash file to create all directories which are ignored by git but needed to work system properly

---
+++ installer.sh
@@ -0,0 +1,9 @@
+#Run this file after composer install
+cd webroot
+mkdir assets
+cd assets
+mkdir about_me
+mkdir categories_gallery
+mkdir favourites_footer
+mkdir gallery
+mkdir top_slider_homepage
